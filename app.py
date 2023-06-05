f4a242b9afad04502abc689c19fcf967ed6ece16
syedhadi209 <hadi.alvi@devsinc.com>
2023-06-05 19:28:23 +0500

    created a website using bootstrap and rendering dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+        "id":1,
+        "title":"web developer",
+        "salary":100000,
+    },
+    {
+        "id":2,
+        "title":"android developer",
+        "salary":150000,
+    },
+    {
+        "id":3,
+        "title":"python developer",
+        "salary":120000,
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return render_template("home.html",jobs=JOBS)
 
 
 
